a824b78a6b370a2eb5416b4192d2f8382d07dcb5
Jesper Udby <jesper@udby.com>
2023-03-12 11:21:55 +0100

    Simplifying the simple

--- binarysearch/src/test/java/com/udby/blog/binarysearch/BinarySearcherSimpleIntegerTest.java
+++ binarysearch/src/test/java/com/udby/blog/binarysearch/BinarySearcherSimpleIntegerTest.java
@@ -9,13 +9,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BinarySearcherSimpleIntegerTest {
     private final BinarySearcher<Integer> binarySearcher = BinarySearcher.ofElements(List.of(3, 1, 2));
 
-    private final BiComparator<Integer, Integer> biComparator = Integer::compare;
-
     @Test
     void binarySearch_elementsFound_returnsIndex() {
-        final var i1 = binarySearcher.binarySearch(1, biComparator);
-        final var i2 = binarySearcher.binarySearch(2, biComparator);
-        final var i3 = binarySearcher.binarySearch(3, biComparator);
+        final var i1 = binarySearcher.binarySearch(1, Integer::compare);
+        final var i2 = binarySearcher.binarySearch(2, Integer::compare);
+        final var i3 = binarySearcher.binarySearch(3, Integer::compare);
 
         assertThat(i1).isEqualTo(0);
         assertThat(i2).isEqualTo(1);
@@ -24,8 +22,8 @@ class BinarySearcherSimpleIntegerTest {
 
     @Test
     void binarySearch_elementsNotFound_returnsInsertionIndex() {
-        final var i1 = binarySearcher.binarySearch(0, biComparator);
-        final var i2 = binarySearcher.binarySearch(4, biComparator);
+        final var i1 = binarySearcher.binarySearch(0, Integer::compare);
+        final var i2 = binarySearcher.binarySearch(4, Integer::compare);
 
         assertThat(i1).isEqualTo(-1);
         assertThat(i2).isEqualTo(-4);

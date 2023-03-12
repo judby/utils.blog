e4d02789410b80537be62f321e826b3e6bfc727e
Jesper Udby <jesper@udby.com>
2023-03-12 11:57:02 +0100

    even the simplest piece of code can contain errors... :-D

--- binarysearch/src/test/java/com/udby/blog/binarysearch/BinarySearcherRangeSimpleTest.java
+++ binarysearch/src/test/java/com/udby/blog/binarysearch/BinarySearcherRangeSimpleTest.java
@@ -1,6 +1,5 @@
 package com.udby.blog.binarysearch;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,8 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BinarySearcherRangeSimpleTest {
     private final BinarySearcher<Range> rangeBinarySearcher = new BinarySearcher<>(new ArrayList<>());
 
-    @BeforeEach
-    void setUp() {
+    {
         rangeBinarySearcher.elements().add(new Range("[10,20)", 10, 20));
         rangeBinarySearcher.elements().add(new Range("[20,30)", 20, 30));
         rangeBinarySearcher.elements().add(new Range("[30,40)", 30, 40));
